69496d1870b60751da4898d2c3fb2ea88c496998
dylanrousseau25-rgb <dylanrousseau25@gmail.com>
2026-09-03 12:44:41 +0200

    chore: align Lot 8 migration timestamp

--- supabase/migrations/20260903104400_lot8_helper_search_path_hardening.sql
+++
@@ -1,2 +0,0 @@
-alter function private.lot8_quality_reduce_steps(jsonb) set search_path = pg_catalog, public, private;
-alter function private.lot8_steps_duration(jsonb) set search_path = pg_catalog, public, private;
